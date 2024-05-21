932aeb6ec530044798a5172994fe98d10cf33133
Arno Dupont <arno.dupont.pro@gmail.com>
2024-05-21 23:10:58 +0200

    up

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,8 @@ const CLASSNAMES = {
   TAB: "tab",
   BUTTONS: "buttons",
   VIEWS: "views",
-  VIEW: "view"
+  VIEW: "view",
+  ICONBTN: "icon-btn",
 }
 
 function emit(emitter: TabGroup | Tab, type: string, args: any[]) {
@@ -140,7 +141,9 @@ class TabGroup extends HTMLElement {
     leftGroup.appendChild(leftButtonContainer);
 
     const previousButton = leftButtonContainer.appendChild(document.createElement("button"));
-    previousButton.innerHTML = "&#8617;";
+    previousButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="m7.85 13l2.85 2.85q.3.3.288.7t-.288.7q-.3.3-.712.313t-.713-.288L4.7 12.7q-.3-.3-.3-.7t.3-.7l4.575-4.575q.3-.3.713-.287t.712.312q.275.3.288.7t-.288.7L7.85 11H19q.425 0 .713.288T20 12t-.288.713T19 13z"/></svg>
+    `;
     previousButton.addEventListener("click", () => {
       const activeTab = this.getActiveTab();
       if (activeTab) {
@@ -150,7 +153,9 @@ class TabGroup extends HTMLElement {
 
     const nextButton = leftButtonContainer.appendChild(document.createElement("button"));
     nextButton.setAttribute("class", CLASSNAMES.BUTTONS);
-    nextButton.innerHTML = "&#8618;";
+    nextButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M16.15 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L19.3 11.3q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7z"/></svg>
+    `;
     nextButton.addEventListener("click", () => {
       const activeTab = this.getActiveTab();
       if (activeTab) {
@@ -159,7 +164,11 @@ class TabGroup extends HTMLElement {
     }, false);
 
     const refreshButton = leftButtonContainer.appendChild(document.createElement("button"));
-    refreshButton.innerHTML = "&#8635;";
+    refreshButton.setAttribute("class", CLASSNAMES.BUTTONS);
+    refreshButton.setAttribute("style", "margin-left: 16px;");
+    refreshButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M6 12.05q0 1.125.425 2.188T7.75 16.2l.25.25V15q0-.425.288-.712T9 14t.713.288T10 15v4q0 .425-.288.713T9 20H5q-.425 0-.712-.288T4 19t.288-.712T5 18h1.75l-.4-.35q-1.3-1.15-1.825-2.625T4 12.05Q4 9.7 5.2 7.787T8.425 4.85q.35-.2.738-.025t.512.575q.125.375-.012.75t-.488.575q-1.45.8-2.312 2.213T6 12.05m12-.1q0-1.125-.425-2.187T16.25 7.8L16 7.55V9q0 .425-.288.713T15 10t-.712-.288T14 9V5q0-.425.288-.712T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6h-1.75l.4.35q1.225 1.225 1.788 2.663T20 11.95q0 2.35-1.2 4.263t-3.225 2.937q-.35.2-.737.025t-.513-.575q-.125-.375.013-.75t.487-.575q1.45-.8 2.313-2.212T18 11.95"/></svg>
+    `;
     refreshButton.addEventListener("click", () => {
       const activeTab = this.getActiveTab();
       if (activeTab) {
@@ -171,9 +180,10 @@ class TabGroup extends HTMLElement {
     rightGroup.setAttribute("class", CLASSNAMES.RIGHTGROUP);
     header.appendChild(rightGroup);
 
-    const unzoomButton = rightGroup.appendChild(document.createElement("button"));
+    const unzoomButton = rightGroup.appendChild(document.createElement("span"));
+    unzoomButton.setAttribute("class", CLASSNAMES.ICONBTN);
     unzoomButton.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 10h6m2 5l6 6m-11-4a7 7 0 1 1 0-14a7 7 0 0 1 0 14"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 10h6m2 5l6 6m-11-4a7 7 0 1 1 0-14a7 7 0 0 1 0 14"/></svg>
     `;
     unzoomButton.addEventListener("click", () => {
       const activeTab = this.getActiveTab();
@@ -182,10 +192,10 @@ class TabGroup extends HTMLElement {
       }
     }, false);
 
-    const zoomButton = rightGroup.appendChild(document.createElement("button"));
-    zoomButton.setAttribute("class", CLASSNAMES.BUTTONS);
+    const zoomButton = rightGroup.appendChild(document.createElement("span"));
+    zoomButton.setAttribute("class", CLASSNAMES.ICONBTN);
     zoomButton.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 10h3m0 0h3m-3 0V7m0 3v3m5 2l6 6m-11-4a7 7 0 1 1 0-14a7 7 0 0 1 0 14"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 10h3m0 0h3m-3 0V7m0 3v3m5 2l6 6m-11-4a7 7 0 1 1 0-14a7 7 0 0 1 0 14"/></svg>
     `;
     zoomButton.addEventListener("click", () => {
       const activeTab = this.getActiveTab();
