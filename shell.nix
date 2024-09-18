e10428b33a58da53096bd0e38bc3438933753230
Arno Dupont <arno.dupont.pro@gmail.com>
2024-09-18 20:29:32 +0200

    up

--- shell.nix
+++ shell.nix
@@ -4,9 +4,9 @@ mkShell {
     nativeBuildInputs = [
         nodejs-18_x
         yarn
-        electron_27
+        electron_30
         wineWowPackages.stable
     ];
 
- ELECTRON_OVERRIDE_DIST_PATH = "${electron_27}/bin/";
+ ELECTRON_OVERRIDE_DIST_PATH = "${electron_30}/bin/";
 }
